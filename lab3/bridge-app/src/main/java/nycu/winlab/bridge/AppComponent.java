17b6f5e076109797e4b606fffaa4a3bf023202d5
Huaish <zsisc.huaish@gmail.com>
2024-10-03 15:15:02 +0800

    feat(lab3): implement Learning Bridge App

--- lab3/bridge-app/src/main/java/nycu/winlab/bridge/AppComponent.java
+++ lab3/bridge-app/src/main/java/nycu/winlab/bridge/AppComponent.java
@@ -16,63 +16,183 @@
 package nycu.winlab.bridge;
 
 import org.onosproject.cfg.ComponentConfigService;
-import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Deactivate;
-import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 import org.osgi.service.component.annotations.ReferenceCardinality;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Dictionary;
-import java.util.Properties;
+import java.util.HashMap;
+import java.util.Map;
 
-import static org.onlab.util.Tools.get;
+import org.onosproject.core.ApplicationId;
+import org.onosproject.core.CoreService;
+import org.onosproject.net.flow.DefaultFlowRule;
+import org.onosproject.net.flow.DefaultTrafficSelector;
+import org.onosproject.net.flow.FlowRule;
+import org.onosproject.net.flow.TrafficSelector;
+import org.onosproject.net.flow.TrafficTreatment;
+import org.onosproject.net.packet.PacketPriority;
+import org.onosproject.net.packet.PacketService;
+import org.onosproject.net.packet.PacketProcessor;
+import org.onosproject.net.packet.PacketContext;
+import org.onosproject.net.packet.InboundPacket;
+
+import org.onlab.packet.Ethernet;
+import org.onlab.packet.MacAddress;
+
+import org.onosproject.net.PortNumber;
+import org.onosproject.net.DeviceId;
+
+import org.onosproject.net.flow.FlowRuleService;
 
 /**
  * Skeletal ONOS application component.
  */
-@Component(immediate = true,
-           service = {SomeInterface.class},
-           property = {
-               "someProperty=Some Default String Value",
-           })
-public class AppComponent implements SomeInterface {
+@Component(immediate = true)
+public class AppComponent {
 
-    private final Logger log = LoggerFactory.getLogger(getClass());
-
-    /** Some configurable property. */
-    private String someProperty;
+    private final Logger log = LoggerFactory.getLogger("LearningBridge");
 
     @Reference(cardinality = ReferenceCardinality.MANDATORY)
     protected ComponentConfigService cfgService;
 
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected CoreService coreService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected PacketService packetService;
+
+    @Reference(cardinality = ReferenceCardinality.MANDATORY)
+    protected FlowRuleService flowRuleService;
+
+    private LearningBridgeProcessor processor = new LearningBridgeProcessor();
+    private ApplicationId appId;
+    private Map<DeviceId, Map<MacAddress, PortNumber>> bridgeTable = new HashMap<>();
+
     @Activate
     protected void activate() {
-        cfgService.registerProperties(getClass());
+        // cfgService.registerProperties(getClass());
+
+        // Register app
+        appId = coreService.registerApplication("nycu.winlab.bridge");
+
+        // Add a packet processor to packetService
+        packetService.addProcessor(processor, PacketProcessor.director(2));
+
+        // Install a flow rule for packet-in
+        TrafficSelector.Builder selectorBuilder = DefaultTrafficSelector.builder();
+        selectorBuilder.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.requestPackets(selectorBuilder.build(), PacketPriority.REACTIVE, appId);
+
         log.info("Started");
     }
 
     @Deactivate
     protected void deactivate() {
-        cfgService.unregisterProperties(getClass(), false);
+        // cfgService.unregisterProperties(getClass(), false);
+
+        // remove flowrule installed by your app
+        flowRuleService.removeFlowRulesById(appId);
+
+        // remove your packet processor
+        packetService.removeProcessor(processor);
+        processor = null;
+
+        // remove flowrule you installed for packet-in
+        TrafficSelector.Builder selector = DefaultTrafficSelector.builder();
+        selector.matchEthType(Ethernet.TYPE_IPV4);
+        packetService.cancelPackets(selector.build(), PacketPriority.REACTIVE, appId);
+
         log.info("Stopped");
     }
 
-    @Modified
-    public void modified(ComponentContext context) {
-        Dictionary<?, ?> properties = context != null ? context.getProperties() : new Properties();
-        if (context != null) {
-            someProperty = get(properties, "someProperty");
+    private class LearningBridgeProcessor implements PacketProcessor {
+
+        @Override
+        public void process(PacketContext context) {
+            // Stop processing if the packet has been handled, since we
+            // can't do any more to it.
+            if (context.isHandled()) {
+                return;
+            }
+            InboundPacket pkt = context.inPacket();
+            Ethernet ethPkt = pkt.parsed();
+
+            if (ethPkt == null) {
+                return;
+            }
+
+            DeviceId recDevId = pkt.receivedFrom().deviceId();
+            PortNumber recPort = pkt.receivedFrom().port();
+            MacAddress srcMac = ethPkt.getSourceMAC();
+            MacAddress dstMac = ethPkt.getDestinationMAC();
+
+            // rec packet-in from new device, create new table for it
+            if (bridgeTable.get(recDevId) == null) {
+                bridgeTable.put(recDevId, new HashMap<>());
+            }
+
+            // TODO1: implement the learning bridge algorithm
+            if (bridgeTable.get(recDevId).get(srcMac) == null) {
+
+                // the mapping of pkt's src mac and receivedfrom port wasn't store in the table
+                // of the rec device
+                bridgeTable.get(recDevId).put(srcMac, recPort);
+                log.info("Add an entry to the port table of `{}`. MAC address: `{}` => Port: `{}`",
+                        recDevId, srcMac, recPort);
+
+            }
+
+            // miss or FLOOD
+            if (bridgeTable.get(recDevId).get(dstMac) == null) {
+                // the mapping of dst mac and forwarding port wasn't store in the table of the
+                // rec device
+                flood(context);
+                log.info("MAC address `{}` is missed on `{}`. Flood the packet.", dstMac, recDevId);
+
+            } else if (bridgeTable.get(recDevId).get(dstMac) != null) {
+                // there is a entry store the mapping of dst mac and forwarding port
+                installRule(context, bridgeTable.get(recDevId).get(dstMac));
+                log.info("MAC address `{}` is matched on `{}`. Install a flow rule.", dstMac, recDevId);
+            }
         }
-        log.info("Reconfigured");
     }
 
-    @Override
-    public void someMethod() {
-        log.info("Invoked");
+    // TODO2: Floods the packet out to all ports except the incoming port
+    private void flood(PacketContext context) {
+        packetOut(context, PortNumber.FLOOD);
+    }
+
+    // TODO3: Sends a packet out to the specified port
+    private void packetOut(PacketContext context, PortNumber port) {
+        context.treatmentBuilder().setOutput(port);
+        context.send();
+    }
+
+    // TODO4: Install a rule forwarding the packet to the specified port
+    private void installRule(PacketContext context, PortNumber port) {
+        Ethernet inPkt = context.inPacket().parsed();
+        TrafficSelector.Builder selectorBuilder = DefaultTrafficSelector.builder();
+
+        selectorBuilder.matchEthDst(inPkt.getDestinationMAC()).matchEthSrc(inPkt.getSourceMAC());
+
+        TrafficTreatment treatment = context.treatmentBuilder().setOutput(port).build();
+
+        FlowRule flowRule = DefaultFlowRule.builder()
+                .withSelector(selectorBuilder.build()) // Traffic selector
+                .withTreatment(treatment) // Action to perform
+                .withPriority(30) // Set a priority for the rule
+                .withIdleTimeout(30) // Timeout for the rule
+                .forDevice(context.inPacket().receivedFrom().deviceId()) // Device where the rule applies
+                .fromApp(appId) // Application ID
+                .build();
+
+        flowRuleService.applyFlowRules(flowRule);
+
+        context.send();
     }
 
 }
